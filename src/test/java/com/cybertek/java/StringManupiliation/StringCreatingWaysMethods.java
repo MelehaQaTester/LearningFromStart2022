2ad91516b495bae12bf666929c4d58651a1a5e38
melehaqaTester <melehayousaf.qa@gmail.com>
2022-11-22 15:56:07 -0500

    changing the folder name

--- src/test/java/com/cybertek/java/StringManupiliation/StringCreatingWaysMethods.java
+++ src/test/java/com/cybertek/java/StringManupiliation/StringCreatingWaysMethods.java
@@ -1,7 +1,7 @@
 package com.cybertek.java.StringManupiliation;
-import java.lang.*;
+import java.lang.*; /**Please import all classes from java.lang*/
 
-public class StringCreatingMethods {
+public class StringCreatingWaysMethods {
     public static void main (String [] args){
        String word  = "patato";
        String word2 = new String ("tamato");
